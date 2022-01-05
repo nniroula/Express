11dceda5df0bee69594fcc715f0491f7d36018d2
nabin <nkbhaie2017@gmail.com>
2022-01-04 17:58:42 -0700

    post request, run in insomnia to get right output

--- expressRoutes.js
+++ expressRoutes.js
@@ -25,6 +25,12 @@ app.get('/dogs', (req, res) => {
     res.send("I am a dog and WOFF WOOF");
 })
 
+// post http verb reques
+app.post('/hello', (req, res)=>{
+    console.log("Back-end coding here please");
+    res.send("Hello coding lovers");
+})
+
 // route handler
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
